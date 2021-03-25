2f154b8a7997c788a896c4e1e550af1fadd6605a
magikitty <elina.satola@gmail.com>
2021-03-25 18:07:17 +0200

    Add Constants file, add gameLoop() and loadLevel() to main.cpp

---
+++ Constants.h
@@ -0,0 +1,6 @@
+#include <string>
+using namespace std;
+
+/* Messages */
+const string MESSAGE_PLAYER_ACTION = "Enter action (wasd to move, p to portal"
+                                     " out): ";
--- main.cpp
+++ main.cpp
@@ -3,18 +3,24 @@
 
 #include <iostream>
 #include "Dungeon.h"
+#include "Player.h"
+#include "Constants.h"
 
 using namespace std;
 
 string getUserInputString(string message);
+void gameLoop(Player player);
+void loadLevel();
 
 int main() {
     cout << "Welcome to Treasure Hunter!" << endl;
-    string namePlayer = getUserInputString("Enter your name: ");
-    cout << "Hello " << namePlayer << "!" << endl;
 
-    Dungeon dungeon(10, 7);
-    dungeon.printDungeon();
+    // Instantiate player, set player name
+    Player player;
+    player.setName(getUserInputString("Enter your name: "));
+    cout << "Hello " << player.getName() << "!" << endl;
+
+    gameLoop(player);
 
     return 0;
 }
@@ -24,4 +30,17 @@ string getUserInputString(string message) {
     cout << message;
     getline(cin, userInput);
     return userInput;
+}
+
+void gameLoop(Player player) {
+    loadLevel();
+    string playerAction;
+    cout << MESSAGE_PLAYER_ACTION;
+    getline(cin, playerAction);
+    player.move(playerAction);
+}
+
+void loadLevel() {
+    Dungeon dungeon(10, 7);
+    dungeon.printDungeon();
 }
