af92e1e8376e84405eeb10dc09a3c6e103a0b5b7
magikitty <elina.satola@gmail.com>
2021-03-25 17:20:22 +0200

    Add welcome message, dungeon printing

--- main.cpp
+++ main.cpp
@@ -2,12 +2,26 @@
 // Programming Languages 2
 
 #include <iostream>
+#include "Dungeon.h"
 
 using namespace std;
 
+string getUserInputString(string message);
+
 int main() {
     cout << "Welcome to Treasure Hunter!" << endl;
+    string namePlayer = getUserInputString("Enter your name: ");
+    cout << "Hello " << namePlayer << "!" << endl;
+
+    Dungeon dungeon(10, 7);
+    dungeon.printDungeon();
 
     return 0;
 }
 
+string getUserInputString(string message) {
+    string userInput;
+    cout << message;
+    getline(cin, userInput);
+    return userInput;
+}
