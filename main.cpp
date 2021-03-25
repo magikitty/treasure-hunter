7de0f9fc11c8c7f46f3b2bff1a1f7c19520c4352
magikitty <elina.satola@gmail.com>
2021-03-25 14:53:29 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Elina Widdowson
+// Programming Languages 2
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to Treasure Hunter!" << endl;
+
+    return 0;
+}
+
