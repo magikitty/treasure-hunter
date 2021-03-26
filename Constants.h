0ced01791ee6c434cd7125a1ddea5650e1742502
magikitty <elina.satola@gmail.com>
2021-03-26 09:33:14 +0200

    Add constants, replace movement strings in Player with constants

--- Constants.h
+++ Constants.h
@@ -1,6 +1,25 @@
 #include <string>
 using namespace std;
 
+/*****************************************************************************/
 /* Messages */
+/*****************************************************************************/
+
 const string MESSAGE_PLAYER_ACTION = "Enter action (wasd to move, p to portal"
                                      " out): ";
+
+/*****************************************************************************/
+/* Movement & player commands */
+/*****************************************************************************/
+
+const string MOVE_UP = "w";
+const string MOVE_LEFT = "a";
+const string MOVE_DOWN = "s";
+const string MOVE_RIGHT = "d";
+const string PORTAL_OUT = "p";
+
+/*****************************************************************************/
+/* Quitting game (quit game if QUIT_GAME bool is true) */
+/*****************************************************************************/
+
+bool QUIT_GAME = false;
--- Player.cpp
+++ Player.cpp
@@ -2,6 +2,7 @@
 // Programming Languages 2
 
 #include "Player.h"
+#include "Constants.h"
 #include <iostream>
 
 std::string Player::getName() const {
@@ -14,15 +15,15 @@ void Player::setName(std::string namePlayer) {
 
 // Dummy function for determining how player moves
 void Player::move(std::string action) {
-    if (action == "w") {
+    if (action == MOVE_UP) {
         std::cout << "You move up!" << std::endl;
-    } else if (action == "a") {
+    } else if (action == MOVE_LEFT) {
         std::cout << "You move left!" << std::endl;
-    } else if (action == "s") {
+    } else if (action == MOVE_DOWN) {
         std::cout << "You move down!" << std::endl;
-    } else if (action == "d") {
+    } else if (action == MOVE_RIGHT) {
         std::cout << "You move right!" << std::endl;
-    } else if (action == "p") {
+    } else if (action == PORTAL_OUT) {
         std::cout << "You portal out of the dungeon!" << std::endl;
     } else {
         std::cout << "Invalid action" << std::endl;
