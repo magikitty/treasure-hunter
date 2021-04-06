882e23d9485886e694aa2dff7f90fadd59b8c820
magikitty <elina.satola@gmail.com>
2021-04-06 16:18:32 +0300

    Add Level class

---
+++ helperClasses/Level.h
@@ -0,0 +1,15 @@
+// Elina Widdowson
+// Programming Languages 2
+
+#ifndef TREASURE_HUNTER_LEVEL_H
+#define TREASURE_HUNTER_LEVEL_H
+
+
+class Level {
+    int size;
+    int movementCost;
+    int levelNumber, levelMaxNumber;
+};
+
+
+#endif //TREASURE_HUNTER_LEVEL_H
