d4c32859bb829e83bcd9cc5c6120484f2dc13b57
magikitty <elina.satola@gmail.com>
2021-04-07 17:15:46 +0300

    Add functions for getUserInput(), quitGame() and startGame()

--- helperClasses/GameManager.cpp
+++ helperClasses/GameManager.cpp
@@ -2,3 +2,35 @@
 // Programming Languages 2
 
 #include "GameManager.h"
+
+using namespace std;
+
+// Prints message and returns user input
+string GameManager::getUserInput(string message) {
+        string userInput;
+        cout << message;
+        getline(cin, userInput);
+        return userInput;
+}
+
+void GameManager::startGame() {
+    cout << MESSAGE_WELCOME << endl;
+
+    // Instantiate player, set player name
+    Player player;
+    player.setName(getUserInput(MESSAGE_ENTER_NAME));
+    cout << "Hello " << player.getName() << "!" << endl;
+}
+
+// Quits game if user portals out or runs out of energy
+void GameManager::quitGame(string playerAction, Player player, EventLogger eventLogger) {
+    if (playerAction == PORTAL_OUT) {
+        cout << MESSAGE_PORTAL_OUT << endl;
+        eventLogger.printEvents();
+        exit(0);
+    } else if (player.getEnergy() == 0) {
+        cout << MESSAGE_PLAYER_DEAD << endl;
+        eventLogger.printEvents();
+        exit(0);
+    }
+}
--- helperClasses/GameManager.h
+++ helperClasses/GameManager.h
@@ -1,16 +1,25 @@
 // Elina Widdowson
 // Programming Languages 2
 
+#include "Level.h"
+#include <iostream>
+#include "../Constants.h"
+#include "../gameObjects/Player.h"
+#include "EventLogger.h"
+
 #ifndef TREASURE_HUNTER_GAMEMANAGER_H
 #define TREASURE_HUNTER_GAMEMANAGER_H
 
 
 class GameManager {
+private:
+    Level level;
 public:
     void gameLoop();
+    std::string getUserInput(std::string message);
     void loadLevel();
+    void quitGame(string playerAction, Player player, EventLogger eventLogger);
     void startGame();
-    void quitGame();
 };
 
 
